fff42372df5df080658492e0f28d52e36b8b2055
nyanyaon <nyanyaonn@gmail.com>
2022-11-12 22:08:28 +0800

    for sudo

--- app/bot/WA.js
+++ app/bot/WA.js
@@ -2,7 +2,10 @@ const qrcode = require('qrcode-terminal');
 const { Client, LocalAuth, Buttons, MessageMedia, NoAuth } = require('whatsapp-web.js');
 
 const client = new Client({
-  authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+		args: ['--no-sandbox'],
+	},
 });
 
 const LIST_CONTACT = [
